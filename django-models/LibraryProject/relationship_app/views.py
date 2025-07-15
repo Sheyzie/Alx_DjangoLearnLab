4db6adc20584d8a3f00030fb2fc4c6c32fbca0cc
Oluwaseyi Olukosi <oluwaseyiolukosi7@gmail.com>
2025-07-15 13:54:36 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
