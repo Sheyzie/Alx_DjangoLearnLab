a2b8068e0c095b4ddc19448c7311c96ffd1da738
Oluwaseyi Olukosi <oluwaseyiolukosi7@gmail.com>
2025-07-18 10:16:31 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth.models import User
 
 
 def admin_check(user):
     return user.is_authenticated and user.role == 'Admin'
 
+@login_required
 @user_passes_test(admin_check)
 def admin_view(request, id):
     admin = User.objects.get(id=id)
