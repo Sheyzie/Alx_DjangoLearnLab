152751786c66da0941bfa1606ba55f1d59b620c6
Oluwaseyi Olukosi <oluwaseyiolukosi7@gmail.com>
2025-07-19 09:38:12 +0100

    Implementing Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views/admin_view.py
+++ django-models/LibraryProject/relationship_app/views/admin_view.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.views.generic import ListView
+from relationship_app.utils import is_admin
 
 from ..models import Librarian, library, Book, Author
 
 
-def admin_check(user):
-    return user.is_authenticated and user.role == 'Admin'
+# def admin_check(user):
+#     return user.is_authenticated and user.role == 'Admin'
 
 # @method_decorator(login_required, name='dispatch')
 # @method_decorator(user_passes_test(admin_check), name='dispatch')
@@ -16,18 +17,23 @@ def admin_check(user):
 #     model = Librarian
 #     context_object_name = 'librarian_list'
 
-@login_required
-@user_passes_test(admin_check)
-def admin_view(request):
-    books = Book.objects.all()
-    authors = Author.objects.all()
-    librarians = Librarian.objects.all()
-    context = {
-        'books': books,
-        'authors': authors,
-        'librarians': librarians,
-    }
-    return render(request, 'admin_view.html', context)
+# @login_required
+# @user_passes_test(admin_check)
+# def admin_view(request):
+#     books = Book.objects.all()
+#     authors = Author.objects.all()
+#     librarians = Librarian.objects.all()
+#     context = {
+#         'books': books,
+#         'authors': authors,
+#         'librarians': librarians,
+#     }
+#     return render(request, 'admin_view.html', context)
+
+
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    return render(request, 'admin_view.html')
 
 
         
