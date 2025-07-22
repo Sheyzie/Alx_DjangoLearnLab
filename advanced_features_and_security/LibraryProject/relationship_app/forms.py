e7fbf7f5d02cb56745f4c8b33fc6265630741f26
Oluwaseyi Olukosi <oluwaseyiolukosi7@gmail.com>
2025-07-22 19:13:50 +0100

    Implementing Security Best Practices in Django(2)

--- advanced_features_and_security/LibraryProject/relationship_app/forms.py
+++ advanced_features_and_security/LibraryProject/relationship_app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser, Author, Librarian, Library, Book, UserProfile
 
+
 class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
