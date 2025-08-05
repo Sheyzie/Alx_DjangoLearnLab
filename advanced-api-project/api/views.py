7e29c4719e88bd7aa494611db12b132261e7a1e6
Oluwaseyi Olukosi <oluwaseyiolukosi7@gmail.com>
2025-08-05 18:13:31 +0100

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
+from .filters import BookFilter
 
 # Create your views here.
 class BookListCreateAPIView(generics.ListCreateAPIView):
@@ -15,13 +18,23 @@ class AuthorListCreateAPIView(generics.ListCreateAPIView):
 
 # for list view
 class BookListView(generics.ListAPIView):
+    '''
+    With django-filter, API calls will be;
+    GET /books/?title=harry&author=rowling&publication_year=2007
+    '''
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'author']
+    filterset_class = BookFilter
 
 # for detail view
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # for create view
 class BookCreateView(generics.CreateAPIView):
@@ -35,9 +48,10 @@ class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
 # for delete view
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
